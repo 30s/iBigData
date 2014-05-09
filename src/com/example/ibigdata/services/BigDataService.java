7fd3562369382d587fa204425f3641ea2d0a882a
Robert <rzheng@xtremeprog.com>
2014-05-09 17:01:56 +0800

    save data to parse

--- src/com/example/ibigdata/services/BigDataService.java
+++ src/com/example/ibigdata/services/BigDataService.java
@@ -8,21 +8,35 @@ import android.content.IntentFilter;
 import android.os.IBinder;
 import android.util.Log;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 public class BigDataService extends Service {
 
 	public static final String TAG = "ibigdata";
-	
+
 	private BroadcastReceiver receiver = new BroadcastReceiver() {
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			String action = intent.getAction();
+			ParseObject usage = new ParseObject("ScreenUsage");
+			usage.put("ts", System.currentTimeMillis());
 			if (action.equals(Intent.ACTION_SCREEN_OFF)) {
 				Log.d(TAG, "screen off " + System.currentTimeMillis());
+				usage.put("val", 0);
 			} else if (action.equals(Intent.ACTION_SCREEN_ON)) {
 				Log.d(TAG, "screen on " + System.currentTimeMillis());
+				usage.put("val", 1);
 			}
+			usage.saveEventually();
 		}
+	};
+
+	@Override
+	public void onCreate() {
+		Parse.initialize(this, "pQZLIr5NXvErXQCAvdW30K9WPHZ92afu7FHdDgZM",
+				"cEb0UIuQc421gFYQexijCjU0xsn3VpJAXbywVxv8");
 	};
 
 	@Override
